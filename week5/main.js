f0b2bcc363608ddad767db14e3c17c87b763f43d
Erin Engler <eng20015@byui.edu>
2021-05-21 20:23:56 -0700

    week 5

---
+++ week5/main.js
@@ -0,0 +1,77 @@
+import ToDoList from "./todolist.js";
+import ToDoItem from "./todoitem.js";
+
+const toDoList = new ToDoList();
+
+// Launch app
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete") {
+        initApp();
+    }
+});
+
+const initApp = () => {
+    // Add listeners
+
+    //Procedural
+    // load list object
+    refreshThePage();
+};
+
+const refreshThePage = () => {
+    clearListDisplay();
+    renderList();
+    clearItemEntryField();
+    setFocusOnItemEntry();
+};
+
+const clearListDisplay = () => {
+    const parentElement = document.getElementById("listItems");
+    deleteContents(parentElement);
+};
+
+const deleteContents = (parentElement) => {
+    let child = parentElement.lastElementChild;
+    while (child) {
+        parentElement.removeChild(child);
+        child = parentElement.lastElementChild;
+    }
+};
+
+const renderList = () => {
+    const list = toDoList.getList();
+    list.forEach((item) => {
+        buildListItem(item);
+    });
+};
+
+const buildListItem = (item) => {
+    const div = document.createElement("div");
+    div.className = "item";
+    const check = document.createElement("input");
+    check.type = "checkbox";
+    check.id = item.getId();
+    check.tabIndex = 0;
+    addClickListenerToCheckbox(check);
+    const label = document.createElement("label");
+    label.htmlFor = item.getId();
+    label.textContent = item.getItem();
+    div.appendChild(check);
+    div.appendChild(label);
+    const container = document.getElementById("listItems");
+    container.appendChild(div);
+};
+
+const addClickListenerToCheckbox = (checkbox) => {
+    checkbox.addEventListener("click", (event) => {
+        toDoList.removeItemFromList(checkbox.id);
+        // TODO remove from persistent data
+        setTimeout(() => {
+            refreshThePage();
+        }, 1000);
+    });
+};
+
+const clearItemEntryField = () => {
+    document.getElementById("newItem").value = "";
+};
