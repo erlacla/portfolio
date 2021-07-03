5fcf71a7d1f73c326fe624896c610229bfb5fdc8
Erin Engler <eng20015@byui.edu>
2021-07-03 16:19:18 -0700

    week 11

--- week11/madLib/scripts/index.js
+++ week11/madLib/scripts/index.js
@@ -3,3 +3,12 @@ import { fetchJSON } from "./fetchJSON.js";
 const url = "https://api.le-systeme-solaire.net/rest/bodies/";
 
 fetchJSON(url);
+
+document.querySelector('#togglebtn').addEventListener("click", toggleMenu);
+function toggleMenu() {
+    document.querySelector("#nav").classList.toggle("hide");
+  }
+
+  const madLibOne = document.createElement('p');
+//   madLibOne.innerHTML = `${} 
+//   ${}`;
