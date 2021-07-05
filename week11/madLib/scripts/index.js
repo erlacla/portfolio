7e896ed6fc5675cd6edd5eaa6d3056b15bbb3af8
Erin Engler <eng20015@byui.edu>
2021-07-05 11:12:55 -0700

    more updates

--- week11/madLib/scripts/index.js
+++ week11/madLib/scripts/index.js
@@ -2,8 +2,12 @@ import { fetchJSON } from "./fetchJSON.js";
 
 const url = "https://api.le-systeme-solaire.net/rest/bodies/";
 
-fetchJSON(url);
+// fetchJSON(url);
 
+const json = await fetchJSON(url);
+//const bodies = JSON.stringify(json);
+
+table();
 document.querySelector('#togglebtn').addEventListener("click", toggleMenu);
 function toggleMenu() {
     document.querySelector("#nav").classList.toggle("hide");
@@ -12,3 +16,27 @@ function toggleMenu() {
   const madLibOne = document.createElement('p');
 //   madLibOne.innerHTML = `${} 
 //   ${}`;
+
+
+
+
+
+
+
+// --- For Solar Objects Table
+
+function table() {
+  alert("function called")
+  let i;
+  console.log(json);
+  
+  let table = "<tr><th>Name</th><th>Radius</th></tr>";
+  
+  for (i = 0; i < json.length; i++) { 
+  //   let row = `<tr><td>${json.bodies[i].englishname}</td><td>${json.bodies[i].equaRadius / 1.609} miles</td></tr>`;
+  // table.appendChild(row);
+       console.log(bodies[i].englishname);
+  }
+  
+  document.getElementById("sso").innerHTML = table;
+}
