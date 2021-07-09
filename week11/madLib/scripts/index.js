308220d09d87509640790881b9b97a323c3d77b6
Erin Engler <eng20015@byui.edu>
2021-07-08 19:33:03 -0700

    madlib progress

--- week11/madLib/scripts/index.js
+++ week11/madLib/scripts/index.js
@@ -5,14 +5,25 @@ const url = "https://api.le-systeme-solaire.net/rest/bodies/";
 fetchJSON(url);
 
 const json = await fetchJSON(url);
-
-document.querySelector('#togglebtn').addEventListener("click", toggleMenu);
+madLib(json);
+document.querySelector("#togglebtn").addEventListener("click", toggleMenu);
 function toggleMenu() {
-    document.querySelector("#nav").classList.toggle("hide");
-  }
+  document.querySelector("#nav").classList.toggle("hide");
+}
 
-  const madLibOne = document.createElement('p');
-//   madLibOne.innerHTML = `${} 
-//   ${}`;
+function madLib(json) {
+  let girlsName = document.querySelector("#g-name").value;
+  let section = document.getElementById("display");
+  let story = document.createElement("p");
 
+  story.innerHTML = `Once upon a time there was a girl named ${girlsName}.`;
 
+  section.appendChild(story);
+}
+
+const today = new Date();
+let currentDay = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+  today
+);
+let date = document.querySelector("#currentdate");
+date.textContent = currentDay;
