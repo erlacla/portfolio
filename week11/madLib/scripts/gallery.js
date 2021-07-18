a640aa45d94f540795757fc1803a75794ec3858e
Erin Engler <eng20015@byui.edu>
2021-07-17 20:02:05 -0700

    week 13 wrk

--- week11/madLib/scripts/gallery.js
+++ week11/madLib/scripts/gallery.js
@@ -4,58 +4,6 @@ function toggleMenu() {
   document.querySelector("#nav").classList.toggle("hide");
 }
 
-function scaleGallery()
-{
-  // This is roughly the max pixels width/height of a square photo
-  let widthSetting = 400;
-  
-  // Do not edit any of this unless you know what you're doing
-  let containerWidth = document.querySelector(".gallery").clientWidth;
-  let ratioSumMax = containerWidth / widthSetting;
-  let imgs = document.querySelectorAll(".gallery img");
-  let numPhotos = imgs.length, ratioSum, ratio, photo, row, rowPadding, i = 0;
-  
-  while (i < numPhotos) {
-    ratioSum = rowPadding = 0;
-    row = new Array();
-    while (i < numPhotos && ratioSum < ratioSumMax) {
-        photo = imgs[i];
-        // reset width to original
-        photo.style.height = ""; 
-        ratio = photo.clientWidth / photo.clientHeight;
-        console.log(photo);
-        rowPadding += getHorizontalPadding(photo);
-        // if this is going to be first in the row, clear: left
-        if(ratioSum == 0) photo.style = ("clear", "left"); else photo.style = ("clear", "none");
-        ratioSum += ratio;
-        row.push(photo);
-        i++;
-        // if only 1 image left, squeeze it in
-        if(i == numPhotos - 1) ratioSumMax = 999;
-    }
-    let unitWidth = (containerWidth - rowPadding) / ratioSum;
-    
-    row.forEach(element => {
-      element.clientWidth(unitWidth * element.clientWidth / element.clientHeight);
-    });
-  }
-}
-
-function getHorizontalPadding(elem)
-{
-    let padding = 0;
-    let left = elem.style = ("padding-left");
-    let right = elem.style = ("padding-right");
-    padding += parseInt(left ? left.replace("px", "") : 0);
-    padding += parseInt(right ? right.replace("px", "") : 0);
-    return padding;
-}
-
-window.onload = scaleGallery;
-window.onresize = scaleGallery;
-
-
-
 let req = new XMLHttpRequest();
 let url = "https://api.nasa.gov/planetary/apod?api_key=";
 let api_key = "EuGKO5mBswmcnrMN8LpU99ziiL5tDIjqx1APfpsw";
@@ -63,9 +11,9 @@ let api_key = "EuGKO5mBswmcnrMN8LpU99ziiL5tDIjqx1APfpsw";
 req.open("GET", url + api_key);
 req.send();
 
-req.addEventListener("load", function() {
-	if(req.status == 200 && req.readyState == 4){
-  	let response = JSON.parse(req.responseText);
+req.addEventListener("load", function () {
+  if (req.status == 200 && req.readyState == 4) {
+    let response = JSON.parse(req.responseText);
 
     let title = response["title"];
     let date = response["date"];
@@ -77,4 +25,31 @@ req.addEventListener("load", function() {
     document.getElementById("pic").src = pic;
     document.getElementById("explanation").innerHTML = explanation;
   }
-})
+});
+
+const current = document.querySelector("#current");
+const imgs = document.querySelector(".imgs");
+const img = document.querySelectorAll(".imgs img");
+const opacity = 0.6;
+
+// Set first img opacity
+img[0].style.opacity = opacity;
+
+imgs.addEventListener("click", imgClick);
+
+function imgClick(e) {
+  // Reset the opacity
+  img.forEach((img) => (img.style.opacity = 1));
+
+  // Change current image to src of clicked image
+  current.src = e.target.src;
+
+  // Add fade in class
+  current.classList.add("fade-in");
+
+  // Remove fade-in class after .5 seconds
+  setTimeout(() => current.classList.remove("fade-in"), 500);
+
+  // Change the opacity to opacity var
+  e.target.style.opacity = opacity;
+}
